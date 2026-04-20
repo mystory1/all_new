0acfa31512355de5dbae21f833f1ea45975668a6
mystory1 <patrikmolnar1@gmail.com>
2026-04-20 20:22:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 // config.js
 window.APP_CONFIG = {
   SUPABASE_URL: 'https://adpsrpsmfhokyqtvldki.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkcHNycHNtZmhva3lxdHZsZGtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0MTM1ODYsImV4cCI6MjA5MDk4OTU4Nn0.CVrGlJpjtKTvIUJ2tHuYvDCs-TzNLHcO91R7GQJy6Y0',
-  APPROVAL_EMAIL_ENDPOINT: "https://adpsrpsmfhokyqtvldki.supabase.co/functions/v1/send-approval-email",
-  APPROVAL_EMAIL_KEY: "mojkod123"
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkcHNycHNtZmhva3lxdHZsZGtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0MTM1ODYsImV4cCI6MjA5MDk4OTU4Nn0.CVrGlJpjtKTvIUJ2tHuYvDCs-TzNLHcO91R7GQJy6Y0'
 };
